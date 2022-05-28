93f35238455bc2b5ff77ad9c2b1d5539719044dd
Riyadhi <faishal.riyadhi@gmail.com>
2022-05-28 14:40:15 +0800

    Struktur Data Array

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const firstUser = {
+    name : "Monica",
+    gender : "Female",
+    age : 17,
+    email : "monica@dingdong.com",
+    favoriteColor : ["Yellow", "Pink", "White", "Purple"],
+    isHavePet : true,
+    education : [
+            {name : "SD 01", city : "Jakarta", graduation : 2016},
+            {name : "SMP 02", city : "Jakarta", graduation : 2019},
+            {name : "SMA 03", city : "Tangerang"},
+        ],
+    favoriteRestaurant : {bento : "HokaHokaBento", sushi : "Sushi Tei", Padang : "Bogaraso"},
+};
+const secondUser = {
+    name : "Wendy",
+    gender : "Male",
+    age : 23,
+    email : "wendy@dingdong.com",
+    favoriteColor : ["Blue", "Black", "Gray"],
+    isHavePet : false,
+    education : [
+        {name : "SD 02", city : "Jakarta", graduation : 2010},
+        {name : "SMP 03", city : "Bogor", graduation : 2013},
+        {name : "SMA 01", city : "Surabaya", graduation : 2016},
+        {name : "Universitas Maju", city : "Tangerang"},
+    ],
+    favoriteRestaurant : {tempura : "Yusukekun", katsu : "Rabenshi", geprek : "KFC"},
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
-
+users.push(firstUser, secondUser);
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
     console.log(users.length || users.size);
